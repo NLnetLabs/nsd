d2e9f3d761cef5d6c9c42350c79bd8513e82b1a0
Alexis Yushin <alexis@nlnetlabs.nl>
2002-05-06 18:55:55 +0000

    *** empty log message ***
    git-svn-id: file:///svn/nsd/trunk@252 a26ef69c-88ff-0310-839f-98b793d9c207

---
+++ dname.c
@@ -0,0 +1,79 @@
+/*
+ * $Id: dname.c,v 1.1 2002/05/06 18:55:55 alexis Exp $
+ *
+ * dname.c -- nsd(8) domain name manipulations
+ *
+ * Alexis Yushin, <alexis@nlnetlabs.nl>
+ *
+ * Copyright (c) 2001, NLnet Labs. All rights reserved.
+ *
+ * This software is an open source.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the NLNET LABS nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#include "nsd.h"
+
+int
+dnamecmp(a, b)
+	register u_char *a;
+	register u_char *b;
+{
+	register int r;
+	register int alen = (int)*a;
+	register int blen = (int)*b;
+
+	while(alen && blen) {
+		a++; b++;
+		if((r = *a - *b)) return r;
+		alen--; blen--;
+	}
+	return alen - blen;
+}
+
+void
+dnameinvert(dname, iname)
+	u_char *dname;
+	u_char *iname;
+{
+
+	u_char *stack[MAXDOMAINLEN];
+	int i;
+
+	for(i = 0, *iname++ = *dname++; *dname; dname += *dname + 1, i++) {
+		stack[i] = dname;
+	}
+
+	while(i) {
+		i--;
+		bcopy(stack[i], iname, *stack[i]+1);
+		iname += *stack[i] + 1;
+	}
+
+	*iname++ = 0;
+}
