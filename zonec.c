98328596994cccc1d497df6ab75799faa9bf1638
Alexis Yushin <alexis@nlnetlabs.nl>
2003-02-18 12:56:48 +0000

    Some minor adjustments in --enable-root-server
    git-svn-id: file:///svn/nsd/trunk@516 a26ef69c-88ff-0310-839f-98b793d9c207

--- zonec.c
+++ zonec.c
@@ -1,5 +1,5 @@
 /*
- * $Id: zonec.c,v 1.77 2003/02/18 11:03:34 alexis Exp $
+ * $Id: zonec.c,v 1.78 2003/02/18 12:56:48 alexis Exp $
  *
  * zone.c -- reads in a zone file and stores it in memory
  *
@@ -445,7 +445,7 @@ zone_read (char *name, char *zonefile)
 #ifndef ROOT_SERVER
 	/* Is it a root zone? Are we a root server then? Idiot proof. */
 	if(dnamecmp(z->dname, (u_char *)"\001") == 0) {
-		fprintf(stderr, "zonec: compile as root server to server the \".\" zone. See documentation\n");
+		fprintf(stderr, "zonec: Not configured as a root serve. See documentation\n");
 		zone_free(z);
 		return NULL;
 	}
