388a7d94b8f7b44a20329a9f0ff4463f2595b097
Alexis Yushin <alexis@nlnetlabs.nl>
2003-03-20 10:58:47 +0000

    *** empty log message ***
    git-svn-id: file:///svn/nsd/trunk@557 a26ef69c-88ff-0310-839f-98b793d9c207

--- zparser.c
+++ zparser.c
@@ -1,5 +1,5 @@
 /*
- * $Id: zparser.c,v 1.28 2003/03/20 10:58:17 alexis Exp $
+ * $Id: zparser.c,v 1.29 2003/03/20 10:58:47 alexis Exp $
  *
  * zparser.c -- master zone file parser
  *
@@ -1795,7 +1795,7 @@ typebyint(u_int16_t type)
 	static char typebuf[] = "TYPEXXXXX";
 	char *t = namebyint(type, ztypes);
 	if(t == NULL) {
-		sprintf(typebuf + 4, sizeof(typebuf) - 4, "%u", type);
+		snprintf(typebuf + 4, sizeof(typebuf) - 4, "%u", type);
 		t = typebuf;
 	}
 	return t;
@@ -1807,7 +1807,7 @@ classbyint(u_int16_t class)
 	char classbuf[] = "CLASSXXXXX";
 	char *t = namebyint(class, zclasses);
 	if(t == NULL) {
-		sprintf(classbuf + 5, sizeof(classbuf) - 5, "%u", class);
+		snprintf(classbuf + 5, sizeof(classbuf) - 5, "%u", class);
 		t = classbuf;
 	}
 	return t;
