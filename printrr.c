a04c2229359f86a91e39115047d4b544b0d3ece9
Miek Gieben <miek@miek.nl>
2004-03-10 10:25:57 +0000

    moved print functions to printrr.c file
    git-svn-id: file:///svn/nsd/trunk@1164 a26ef69c-88ff-0310-839f-98b793d9c207

---
+++ printrr.c
@@ -0,0 +1,362 @@
+/*
+ * printrr.c -- print RRs
+ *
+ * Copyright (c) 2001-2004, NLnet Labs. All rights reserved.
+ *
+ * See LICENSE for the license.
+ *
+ */
+
+#include <config.h>
+
+#include <assert.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+#include <unistd.h>
+#include <time.h>
+
+#include <netinet/in.h>
+
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif
+
+#include "printrr.h"
+
+/* [XXX check if these are needed */
+#include "dname.h"
+#include "dns.h"
+#include "namedb.h"
+#include "util.h"
+#include "region-allocator.h"
+
+/* print a RR */
+int
+print_rr(rr_type *rr)
+{
+	uint32_t ttl;
+	uint8_t *owner;
+	uint16_t *r;
+	uint8_t *type, *typecovered;
+	int i;	/* counter */
+	char *j; /* unknown rr */
+	
+	owner	= (uint8_t*)dname_to_string( domain_dname(rr->domain) );
+	ttl	= rr->rrdata->ttl;
+	type	= (uint8_t *)namebyint(rr->type,ztypes);
+
+	if ( type == NULL ) {
+		/* no type found, must be unknown */
+		type = (uint8_t *) malloc(10);
+		/* should not exceed 5 digits */
+		sprintf(type, "TYPE%d", rr->type);
+	}
+	
+	printf("%s\t%ld IN %s\t", owner, ttl, type);
+
+        switch (rr->type) {
+		case TYPE_A:
+			printf("%s",wire_conv_a(rr->rrdata->rdata[0]));
+			break;
+		case TYPE_AAAA:
+			printf("%s",wire_conv_aaaa(rr->rrdata->rdata[0]));
+			break;
+		case TYPE_CNAME:
+                case TYPE_NS:
+			printf("%s",wire_conv_domain(rr->rrdata->rdata[0]));
+                        break;
+		case TYPE_MX:
+			printf("%d %s",wire_conv_short(rr->rrdata->rdata[0]),
+					wire_conv_domain(rr->rrdata->rdata[1]));
+			break;
+		case TYPE_SOA:
+			printf("%s %s %d %d %d %d %d",
+				 	wire_conv_domain(rr->rrdata->rdata[0]),
+					wire_conv_domain(rr->rrdata->rdata[1]),
+					wire_conv_long(rr->rrdata->rdata[2]),
+					wire_conv_long(rr->rrdata->rdata[3]),
+					wire_conv_long(rr->rrdata->rdata[4]),
+					wire_conv_long(rr->rrdata->rdata[5]),
+					wire_conv_long(rr->rrdata->rdata[6]));
+			break;
+		case TYPE_DNSKEY:
+			printf("%d %d %d %s",
+					wire_conv_short(rr->rrdata->rdata[0]),
+					wire_conv_byte(rr->rrdata->rdata[1]),
+					wire_conv_byte(rr->rrdata->rdata[2]),
+					wire_conv_b64(rr->rrdata->rdata[3]));
+			break;
+		case TYPE_NSEC:
+			printf("%s",
+					wire_conv_labels(rr->rrdata->rdata[0]));
+			break;
+		case TYPE_RRSIG:
+			typecovered = (uint8_t *)namebyint(
+					wire_conv_rrtype(rr->rrdata->rdata[0]),ztypes);
+			if ( typecovered == NULL ) {
+				typecovered = (uint8_t *) malloc(10);
+				sprintf(typecovered, "TYPE%d", 
+						wire_conv_rrtype(rr->rrdata->rdata[0]));
+			}
+	
+			printf("%s %d %d %ld %s %s %d %s %s",
+					typecovered,
+					wire_conv_byte(rr->rrdata->rdata[1]),
+					wire_conv_byte(rr->rrdata->rdata[2]),
+					wire_conv_long(rr->rrdata->rdata[3]),
+					wire_conv_time(rr->rrdata->rdata[4]),
+					wire_conv_time(rr->rrdata->rdata[5]),
+					wire_conv_short(rr->rrdata->rdata[6]),
+					wire_conv_labels(rr->rrdata->rdata[7]),
+					wire_conv_b64(rr->rrdata->rdata[8]));
+			break;	
+		case TYPE_DS:
+			printf("%d %d %d %s",
+					wire_conv_short(rr->rrdata->rdata[0]),
+					wire_conv_byte(rr->rrdata->rdata[1]),
+					wire_conv_byte(rr->rrdata->rdata[2]),
+					wire_conv_hex(rr->rrdata->rdata[3]));
+			break;		
+		case TYPE_TXT:
+			/* [XXX] need to loop */
+			printf("%s",
+					wire_conv_string(rr->rrdata->rdata[0]));
+			break;
+		case TYPE_HINFO:
+			printf("\"%s\" \"%s\"",
+					wire_conv_string(rr->rrdata->rdata[0]),
+					wire_conv_string(rr->rrdata->rdata[1]));
+			break;
+		default:
+			/* print as hex */
+			/* todo, looping */
+			printf("\\# %d ",rdata_atom_size(rr->rrdata->rdata[0]));
+			/* todo print hex */
+			printf("%s",wire_conv_hex(rr->rrdata->rdata[0]));
+        }
+
+	printf("\n");
+
+        return 0;
+}
+
+uint8_t *
+wire_conv_domain(rdata_atom_type a)
+{
+	/* convert from wireformat to a printable owner name
+	 * only works if  rdata_atom_is_domain() is true */
+
+	uint8_t *r = malloc(rdata_atom_size(a) + 1 );
+
+	strcpy(r, (uint8_t*)dname_to_string(domain_dname(rdata_atom_domain(a))));
+
+	return r;
+}
+
+uint8_t *
+wire_conv_labels(rdata_atom_type a)
+{
+	/* convert from wireformat to a printable owner name
+	 * only works if rdata_atom_is_domain() is false */
+	
+	uint8_t *r;
+	uint8_t *s,*o;
+	unsigned int l;
+
+	/* with dots? TODO */
+	s = (uint8_t*) malloc(MAXDOMAINLEN + 1);
+
+	o = s; /* remember for later */
+
+	/* start of the labels */
+	r = (uint8_t*)rdata_atom_data(a);
+
+	while ( *r != 0 ) {
+
+		/* copy */
+		memcpy(s, (r+1), *r);
+		/* insert . */
+		*(s + *r) = '.';
+		*(s + *r + 1) = '\0';
+
+		s = s + 1 + *r;
+		r = r + 1 + *r; /* move to next */
+
+	}
+
+	s    = o; /* rewind */
+
+	return s;
+}
+
+
+uint8_t *
+wire_conv_string(rdata_atom_type a)
+{
+	/* convert strings (TXT, HINFO), to printable string */
+
+	uint8_t *r;
+
+	r = (uint8_t*)rdata_atom_data(a);
+
+	return r;
+}
+
+int
+wire_conv_short(rdata_atom_type a)
+{
+	/* convert from wireformat to a short int */
+	uint16_t *r;
+
+	r = (uint16_t *)rdata_atom_data(a);
+
+	return ( ntohs(*r) );
+}
+
+long int 
+wire_conv_long(rdata_atom_type a)
+{
+	/* convert from wireformat to a long int */
+	/* [XXX] not endian safe! */
+	uint32_t *r;
+	
+	r = (uint32_t *)rdata_atom_data(a);
+
+	return ( ntohl(*r) );
+}
+
+uint8_t *
+wire_conv_a(rdata_atom_type a)
+{
+	/* convert from wireformat to a ip address */
+
+	uint16_t *r;
+	uint8_t  *dst;
+
+	dst = malloc(INET_ADDRSTRLEN);
+
+	r = (uint16_t *)rdata_atom_data(a);
+
+	inet_ntop(AF_INET, r, dst, INET_ADDRSTRLEN);
+	
+	return dst;
+}
+
+uint8_t *
+wire_conv_aaaa(rdata_atom_type a)
+{
+	/* convert from wire to AAAA */
+	uint8_t *dst;
+	uint16_t *r = NULL;
+
+	dst = malloc(INET6_ADDRSTRLEN);
+
+	r = (uint16_t *)rdata_atom_data(a);
+	
+        inet_ntop(AF_INET6, r, dst, INET6_ADDRSTRLEN);
+
+	return dst;
+}
+
+uint8_t *
+wire_conv_b64(rdata_atom_type a)
+{
+	/* convert wire to b64 string */
+
+	uint8_t *buffer;
+	uint16_t *r;
+	int i = rdata_atom_size(a);
+
+	r = (uint16_t *) rdata_atom_data(a);
+	buffer = (uint8_t*) malloc(B64BUFSIZE);
+	
+	/*b64_ntop((uint8_t *) r, (size_t) i, char *target, size_t targsize); */
+	b64_ntop( (uint8_t *)r, i, buffer, B64BUFSIZE);
+
+	return buffer;
+}
+
+short int
+wire_conv_byte(rdata_atom_type a)
+{
+	/* convert wire to byte value */
+	
+	return ( (short int) *( (uint8_t*)rdata_atom_data(a)));
+}
+
+uint8_t *
+wire_conv_hex(rdata_atom_type a)
+{
+	/* convert wire to hex data */
+	int len; int i,j,pos;
+	char h[] = {'0','1','2','3','4','5','6','7','8','9',
+		'A','B','C','D','E','F'};
+	uint8_t *hex;
+	uint8_t *p;
+
+	/* per byte 2 hex numbers */
+	len = rdata_atom_size(a);
+
+	hex = (uint8_t *) malloc( len * 2 + 1);
+	
+	p   = (uint8_t*)rdata_atom_data(a);
+
+	j = 0;
+	for (i=0; i < len; ++i) {
+
+		hex[j]   = h[*p >> 4];
+		hex[++j] = h[*p & 0x0f];
+
+		p++; ++j;
+	}
+	hex[++j] = '\0';
+	return hex;
+}
+
+uint8_t *
+wire_conv_time(rdata_atom_type a)
+{
+	/* convert wire timeformat to long */
+
+        uint32_t *r;
+        uint32_t l;
+	const time_t * timep;
+        struct tm *tm; 
+	uint8_t *s;
+
+	tm = (struct tm*) malloc(sizeof(struct tm));
+
+	r = rdata_atom_data(a);
+
+	l = ntohl(*r);
+	timep = (time_t*)&l;
+
+	gmtime_r( timep, tm );
+
+	s = (uint8_t*)malloc(16);
+
+	strftime(s, 15, "%Y%m%d%H%M%S" , tm);
+
+        return s;
+}
+
+/* uint? */
+uint16_t
+wire_conv_rrtype(rdata_atom_type a)
+{
+	/* convert wire rrtype to int */
+
+	uint16_t *r;
+
+	r = rdata_atom_data(a);
+	
+	return (ntohs(*r));
+
+}
--- zonec.h
+++ zonec.h
@@ -186,21 +186,6 @@ void zprintrr(FILE *f, rr_type *rr);
 void set_bit(uint8_t bits[], int index);
 void set_bitnsec(uint8_t bits[NSEC_WINDOW_COUNT][NSEC_WINDOW_BITS_SIZE], int index);
 
-/* print_rr and helper functions */
-uint8_t * wire_conv_string(rdata_atom_type);
-uint8_t * wire_conv_domain(rdata_atom_type);
-uint8_t * wire_conv_labels(rdata_atom_type);
-uint8_t * wire_conv_a(rdata_atom_type);
-uint8_t * wire_conv_aaaa(rdata_atom_type);
-uint8_t * wire_conv_b64(rdata_atom_type);
-uint8_t * wire_conv_hex(rdata_atom_type);
-uint8_t * wire_conv_time(rdata_atom_type a);
-uint16_t wire_conv_rrtype(rdata_atom_type a);
-long int wire_conv_long(rdata_atom_type);
-short int wire_conv_byte(rdata_atom_type);
-int wire_conv_short(rdata_atom_type);
-int print_rr(rr_type *);
-
 /* zlexer.lex */
 int zoctet(char *word);
 int zrrtype (char *word);
