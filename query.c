fc944b1d267453ca0ced7e01663e9e5d56c9d96f
Erik Rozendaal <erik@nlnetlabs.nl>
2004-10-28 15:13:21 +0000

    Merged changes from the NSD_2_1 branch.
    git-svn-id: file:///svn/nsd/trunk@1317 a26ef69c-88ff-0310-839f-98b793d9c207

--- query.c
+++ query.c
@@ -886,7 +886,11 @@ query_prepare_response(query_type *q)
 	
 	/* Update the flags.  */
 	flags = FLAGS(q);
+#ifdef DNSSEC
 	flags &= 0x0110U;	/* Preserve the RD and CD flags.  */
+#else
+	flags &= 0x0100U;	/* Preserve the RD flag.  */
+#endif
 	flags |= 0x8000U;	/* Set the QR flag.  */
 	FLAGS_SET(q, flags);
 }
