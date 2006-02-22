bcbfeb36fcb0b2781f619de074f348757b785b3a
Miek Gieben <miek@miek.nl>
2006-02-22 12:24:12 +0000

    added cutest added an always_fail test to check cutest
    no tpkg added at this time
    
    svn does weird in my working repo
    
    
    git-svn-id: file:///svn/nsd/trunk@1833 a26ef69c-88ff-0310-839f-98b793d9c207

---
+++ tpkg/unitest/cutest_run.c
@@ -0,0 +1,41 @@
+/*
+	run the unit tests
+	log
+	31 jan 06 (WW): created file.
+	21 feb 06 (MG): reworked for cutest
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "tpkg/cutest/CuTest.h"
+
+CuSuite * reg_cutest_rbtree(void);
+
+/* dummy functions to link */
+struct nsd;
+int writepid(struct nsd * ATTR_UNUSED(nsd))
+{
+	return 0;
+}
+void bind8_stats(struct nsd * ATTR_UNUSED(nsd))
+{
+}
+
+void runalltests(void)
+{
+	CuSuite *suite = reg_cutest_rbtree();
+	CuString *output = CuStringNew();
+
+	CuSuiteRun(suite);
+        CuSuiteSummary(suite, output);
+        CuSuiteDetails(suite, output);
+        printf("%s\n", output->buffer);
+}
+
+int main(int argc, const char *argv[])
+{
+	runalltests();
+}
