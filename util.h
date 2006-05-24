70c0912ef09140df9e13593954550a83ad8007e6
Wouter Wijngaards <wouter@nlnetlabs.nl>
2006-05-24 08:01:47 +0000

    stack type; simple and safe implementation
    git-svn-id: file:///svn/nsd/trunk@2145 a26ef69c-88ff-0310-839f-98b793d9c207

--- util.h
+++ util.h
@@ -326,4 +326,17 @@ int print_rr(FILE *out, struct state_pretty_rr* state, struct rr *record);
  */
 const char* rcode2str(int rc);
 
+/* 
+ * Stack of pointers.
+ * Stack is fixed size on start. More elems fall off stack.
+ */
+struct stack {
+	void** data;
+	size_t num, capacity;
+};
+typedef struct stack stack_type;
+stack_type* stack_create(struct region* region, size_t size);
+void stack_push(stack_type* stack, void* elem);
+void* stack_pop(stack_type* stack);
+
 #endif /* _UTIL_H_ */
