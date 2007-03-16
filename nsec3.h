ecdf1e24ecc7df0b4ca15fd5e7484a2e2bff3a5d
Wouter Wijngaards <wouter@nlnetlabs.nl>
2007-03-16 13:54:17 +0000

    nsec3 comment changes due to review.
    git-svn-id: file:///svn/nsd/trunk@2602 a26ef69c-88ff-0310-839f-98b793d9c207

--- nsec3.h
+++ nsec3.h
@@ -42,31 +42,41 @@ int nsec3_find_cover(struct namedb* db, struct zone* zone,
 	const struct dname* hashname, struct domain** result);
 
 /*
- * Routines used to add the correct nsec3 record to a query answer.
+ * _answer_ Routines used to add the correct nsec3 record to a query answer.
  * cnames etc may have been followed, hence original name.
  */
-/* add proof for wildcards that the name below the wildcard.parent
+/* 
+ * add proof for wildcards that the name below the wildcard.parent
  * does not exist 
  */
 void nsec3_answer_wildcard(struct query *query, struct answer *answer,
         struct domain *wildcard, struct namedb* db,
 	const struct dname *qname);
-/* add NSEC3 to provide domain name but not rrset exists,
+
+/* 
+ * add NSEC3 to provide domain name but not rrset exists,
  * this could be a query for a DS or NSEC3 type
  */
 void nsec3_answer_nodata(struct query *query, struct answer *answer, 
 	struct domain *original);
-/* add NSEC3 for a delegation (optout stuff)
-*/
+
+/* 
+ * add NSEC3 for a delegation (optout stuff)
+ */
 void nsec3_answer_delegation(struct query *query, struct answer *answer);
-/* authoritative answers.
-  *match==0 is an nxdomain. */
+
+/* 
+ * add NSEC3 for authoritative answers.
+ * match==0 is an nxdomain. 
+ */
 void nsec3_answer_authoritative(struct domain** match, struct query *query, 
 	struct answer *answer, struct domain* closest_encloser, 
 	struct namedb* db, const struct dname* qname);
 
-/* true of domain is a NSEC3 (+RRSIG) data only variety.
-   pass nonNULL zone to filter for particular zone. */
+/* 
+ * True if domain is a NSEC3 (+RRSIG) data only variety.
+ * pass nonNULL zone to filter for particular zone. 
+ */
 int domain_has_only_NSEC3(struct domain* domain, struct zone* zone);
 
 #endif /* NSEC3 */
