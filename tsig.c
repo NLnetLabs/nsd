ba52cb0036ddbb1ace76817dc367b50738f00454
Wouter Wijngaards <wouter@nlnetlabs.nl>
2011-01-27 15:44:03 +0000

    fix svn:NO TEST
    git-svn-id: file:///svn/nsd/branches/NSD_3_2_DENIC@3138 a26ef69c-88ff-0310-839f-98b793d9c207

--- tsig.c
+++ tsig.c
@@ -282,7 +282,7 @@ tsig_init_record(tsig_record_type *tsig,
 	tsig->prior_mac_size = 0;
 	tsig->prior_mac_data = NULL;
 #ifdef MEMCHECK
-	regcheck_mark_ignore(tsig->rr_region);
+	regcheck_mark_ignore(tsig->context_region);
 #endif
 	region_free_all(tsig->context_region);
 }
