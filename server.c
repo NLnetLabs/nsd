5a55d139941223c2a6b389f788ae6016feed99b3
Matthijs Mekking <matje@nlnetlabs.nl>
2011-03-08 15:17:25 +0000

    and in branch
    git-svn-id: file:///svn/nsd/branches/NSD_3_2@3220 a26ef69c-88ff-0310-839f-98b793d9c207

--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 /*
  * server.c -- nsd(8) network input/output
  *
- * Copyright (c) 2001-2006, NLnet Labs. All rights reserved.
+ * Copyright (c) 2001-2011, NLnet Labs. All rights reserved.
  *
  * See LICENSE for the license.
  *
