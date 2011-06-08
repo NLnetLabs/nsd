a04e9e7754df0b4e8ef22eff4ae85c55d74765ed
Wouter Wijngaards <wouter@nlnetlabs.nl>
2011-06-08 15:13:37 +0000

    - fixed bug where not all references were removed before mmap was         synced to another process. svn:NO TEST
    git-svn-id: file:///svn/nsd/trunk@3375 a26ef69c-88ff-0310-839f-98b793d9c207

--- ipc.c
+++ ipc.c
@@ -395,6 +395,7 @@ static void
 xfrd_send_reload_req(xfrd_state_t* xfrd)
 {
 	sig_atomic_t req = NSD_RELOAD;
+	udb_ptr_unlink(xfrd->last_task, xfrd->nsd->task[xfrd->nsd->mytask]);
 	task_process_sync(xfrd->nsd->task[xfrd->nsd->mytask]);
 	/* ask server_main for a reload */
 	if(write(xfrd->ipc_handler.fd, &req, sizeof(req)) == -1) {
@@ -406,7 +407,6 @@ xfrd_send_reload_req(xfrd_state_t* xfrd)
 	}
 	DEBUG(DEBUG_IPC,1, (LOG_INFO, "xfrd: asked nsd to reload new updates"));
 	/* swapped task to other side, start to use other task udb. */
-	udb_ptr_unlink(xfrd->last_task, xfrd->nsd->task[xfrd->nsd->mytask]);
 	xfrd->nsd->mytask = 1 - xfrd->nsd->mytask;
 	task_remap(xfrd->nsd->task[xfrd->nsd->mytask]);
 	udb_ptr_init(xfrd->last_task, xfrd->nsd->task[xfrd->nsd->mytask]);
