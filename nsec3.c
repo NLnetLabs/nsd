e634cfff246a31cf6f5e7a835bab793581d06d76
Matthijs Mekking <matje@nlnetlabs.nl>
2011-09-26 10:04:47 +0000

    Don't return SERVFAIL on a domain that looks like a NSEC3 domain but is actually a empty non-terminal.
    git-svn-id: file:///svn/nsd/trunk@3459 a26ef69c-88ff-0310-839f-98b793d9c207

--- nsec3.c
+++ nsec3.c
@@ -741,6 +741,7 @@ nsec3_answer_nodata(struct query *query, struct answer *answer,
 {
 	if(!query->zone->nsec3_param)
 		return;
+
 	/* nodata when asking for secure delegation */
 	if(query->qtype == TYPE_DS)
 	{
@@ -759,18 +760,22 @@ nsec3_answer_nodata(struct query *query, struct answer *answer,
 	else if (original==original->wildcard_child_closest_match
 		&& label_is_wildcard(dname_name(domain_dname(original)))) {
 		/* denial for wildcard is already there */
+
 		/* add parent proof to have a closest encloser proof for wildcard parent */
+		/* in other words: nsec3 matching closest encloser */
 		if(original->parent && original->parent->nsec3_is_exact)
 			nsec3_add_rrset(query, answer, AUTHORITY_SECTION,
 				original->parent->nsec3_cover);
 		/* proof for wildcard itself */
+		/* in other words: nsec3 matching source of synthesis */
 		nsec3_add_rrset(query, answer, AUTHORITY_SECTION,
 			original->nsec3_cover);
 	}
 	else {	/* add nsec3 to prove rrset does not exist */
-		if(original->nsec3_is_exact)
+		if(original->nsec3_is_exact) {
 			nsec3_add_rrset(query, answer, AUTHORITY_SECTION,
 				original->nsec3_cover);
+		}
 	}
 }
 
@@ -813,7 +818,8 @@ nsec3_answer_authoritative(struct domain** match, struct query *query,
 		return;
 	assert(match);
 	/* there is a match, this has 1 RRset, which is NSEC3, but qtype is not. */
-	if(*match &&
+        /* !is_existing: no RR types exist at the QNAME, nor at any descendant of QNAME */
+	if(*match && !(*match)->is_existing &&
 #if 0
 		query->qtype != TYPE_NSEC3 &&
 #endif
@@ -835,6 +841,16 @@ nsec3_answer_authoritative(struct domain** match, struct query *query,
 			/* wildcard and nsec3 domain clash. server failure. */
 			RCODE_SET(query->packet, RCODE_SERVFAIL);
 		}
+		return;
+	}
+	else if(*match && (*match)->is_existing &&
+#if 0
+		query->qtype != TYPE_NSEC3 &&
+#endif
+		domain_has_only_NSEC3(*match, query->zone))
+	{
+		/* this looks like a NSEC3 domain, but is actually an empty non-terminal. */
+		nsec3_answer_nodata(query, answer, *match);
 		return;
 	}
 	if(!*match) {
