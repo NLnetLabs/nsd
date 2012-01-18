26e02d6e2f555993dc049b41122dafd3085e89a0
Wouter Wijngaards <wouter@nlnetlabs.nl>
2012-01-18 16:09:24 +0000

    set rr_count while rrset pointer is valid.
    git-svn-id: file:///svn/nsd/trunk@3506 a26ef69c-88ff-0310-839f-98b793d9c207

--- difffile.c
+++ difffile.c
@@ -261,6 +261,7 @@ rrset_delete(namedb_type* db, domain_type* domain, rrset_type* rrset)
 		add_rdata_to_recyclebin(db, &rrset->rrs[i]);
 	region_recycle(db->region, rrset->rrs,
 		sizeof(rr_type) * rrset->rr_count);
+	rrset->rr_count = 0;
 	region_recycle(db->region, rrset, sizeof(rrset_type));
 
 	/* is the node now an empty node (completely deleted) */
@@ -279,7 +280,6 @@ rrset_delete(namedb_type* db, domain_type* domain, rrset_type* rrset)
 			}
 		}
 	}
-	rrset->rr_count = 0;
 }
 
 static int
