6a7f60f9b3f32bd3f5ccdc51c80b89c66b4f838d
Wouter Wijngaards <wouter@nlnetlabs.nl>
2012-12-18 09:36:46 +0000

    create sha256. (svn:NO TEST)
    git-svn-id: file:///svn/nsd/trunk@3769 a26ef69c-88ff-0310-839f-98b793d9c207

--- makedist.sh
+++ makedist.sh
@@ -192,16 +192,21 @@ cleanup
 case $OSTYPE in
         linux*)
                 sha=`sha1sum nsd-$version.tar.gz |  awk '{ print $1 }'`
+                sha256=`sha256sum nsd-$version.tar.gz |  awk '{ print $1 }'`
                 ;;
         FreeBSD*)
                 sha=`sha1  nsd-$version.tar.gz |  awk '{ print $5 }'`
+                sha256=`sha256  nsd-$version.tar.gz |  awk '{ print $5 }'`
                 ;;
 	*)
                 sha=`sha1sum nsd-$version.tar.gz |  awk '{ print $1 }'`
+                sha256=`sha256sum nsd-$version.tar.gz |  awk '{ print $1 }'`
                 ;;
 esac
 echo $sha > nsd-$version.tar.gz.sha1
+echo $sha256 > nsd-$version.tar.gz.sha256
 
 info "NSD distribution created successfully."
 info "SHA1sum: $sha"
+info "SHA256sum: $sha256"
 
