5e34446b9c2f8151c79b0d0fc97ef87a2547eb15
Wouter Wijngaards <wouter@nlnetlabs.nl>
2016-10-18 07:13:32 +0000

    - Robust fix against missing master in tcp_open for xfrd.
    git-svn-id: file:///svn/nsd/trunk@4710 a26ef69c-88ff-0310-839f-98b793d9c207

--- xfrd-tcp.c
+++ xfrd-tcp.c
@@ -490,6 +490,14 @@ xfrd_tcp_open(xfrd_tcp_set_t* set, struct xfrd_tcp_pipeline* tp,
 	int fd, family, conn;
 	struct timeval tv;
 	assert(zone->tcp_conn != -1);
+
+	/* if there is no next master, fallback to use the first one */
+	/* but there really should be a master set */
+	if(!zone->master) {
+		zone->master = zone->zone_options->pattern->request_xfr;
+		zone->master_num = 0;
+	}
+
 	DEBUG(DEBUG_XFRD,1, (LOG_INFO, "xfrd: zone %s open tcp conn to %s",
 		zone->apex_str, zone->master->ip_address_spec));
 	tp->tcp_r->is_reading = 1;
