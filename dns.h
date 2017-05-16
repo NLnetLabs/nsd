d639ebfbf43f60e16b06e1b8069b0f5cfd5900c7
Wouter Wijngaards <wouter@nlnetlabs.nl>
2017-05-16 14:58:01 +0000

    - zone parser parses type AVC (it has TXT format).
    git-svn-id: file:///svn/nsd/trunk@4753 a26ef69c-88ff-0310-839f-98b793d9c207

--- dns.h
+++ dns.h
@@ -158,6 +158,7 @@ typedef enum nsd_rc nsd_rc_type;
 #define TYPE_ANY	255	/* any type (wildcard) */
 #define TYPE_URI	256	/* RFC 7553 */
 #define TYPE_CAA	257	/* RFC 6844 */
+#define TYPE_AVC	258
 
 #define TYPE_DLV	32769	/* RFC 4431 */
 #define PSEUDO_TYPE_DLV	RRTYPE_DESCRIPTORS_LENGTH
@@ -262,7 +263,7 @@ typedef struct rrtype_descriptor rrtype_descriptor_type;
  *
  * CAA + 1
  */
-#define RRTYPE_DESCRIPTORS_LENGTH  (TYPE_CAA + 1)
+#define RRTYPE_DESCRIPTORS_LENGTH  (TYPE_AVC + 1)
 rrtype_descriptor_type *rrtype_descriptor_by_name(const char *name);
 rrtype_descriptor_type *rrtype_descriptor_by_type(uint16_t type);
 
