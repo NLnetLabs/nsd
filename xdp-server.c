156d436c6534a5e06ba98fe1b822a0ca924617d2
mb <mb@64k.by>
2020-10-16 14:55:49 +0200

    xdp: get interface index and mac address

--- xdp-server.c
+++ xdp-server.c
@@ -9,6 +9,7 @@
 #include "config.h"
 
 #include "region-allocator.h"
+#include "util.h"
 
 #include "xdp-dissect.h"
 #include "xdp-server.h"
@@ -77,6 +78,29 @@ static int eth_dev_promiscuous_disable( xdp_server_type* xdp ) {
 	return eth_dev_change_flags( xdp->_options._interface_name, 0u, ~IFF_PROMISC );
 }
 
+static int eth_dev_info( xdp_server_type* xdp ) {
+	int sock = socket( AF_INET, SOCK_DGRAM, IPPROTO_IP );
+	if( sock < 0 ) return -1;
+
+	struct ifreq ifr;
+	strlcpy( ifr.ifr_name, xdp->_options._interface_name, IFNAMSIZ );
+	if( ioctl( sock, SIOCGIFINDEX, &ifr ) ) goto error;
+
+	xdp->_interface_index = ifr.ifr_ifindex;
+
+	if( ioctl( sock, SIOCGIFHWADDR, &ifr ) ) goto error;
+
+	memcpy( xdp->_interface_hardware_address, ifr.ifr_hwaddr.sa_data,
+		XDP_ETHER_ADDR_LEN );
+
+	close( sock );
+	return 0;
+
+error:
+	close( sock );
+	return -1;
+}
+
 static int ethtool_channels_get(
 	char const* if_name, u32* max_queues, u32* combined_queues ) {
 	struct ethtool_channels channels;
@@ -117,6 +141,34 @@ out:
 
 int xdp_server_init( xdp_server_type* xdp ) {
 	(void)xdp;
+
+	u32 max_queues = 0;
+	u32 combined_queues = 0;
+
+	int rc_info = eth_dev_info( xdp );
+	if( rc_info != 0 ) {
+		log_msg( LOG_ERR, "failed: eth_dev_info(): %s\n", strerror( -errno ) );
+		return rc_info;
+	}
+
+	char mac[32];
+	uint8_t const* p = xdp->_interface_hardware_address;
+	snprintf( mac, 32, "%02x:%02x:%02x:%02x:%02x:%02x",
+	          p[0], p[1], p[2], p[3], p[4], p[5] );
+	log_msg( LOG_NOTICE, "xdp.interface_index=%d xdp.interface_hardware_address=%s",
+		xdp->_interface_index, mac );
+
+	int rc_queues = ethtool_channels_get(
+		xdp->_options._interface_name, &max_queues, &combined_queues );
+	if( rc_queues != 0 ) {
+		log_msg( LOG_ERR, "failed: ethtool_channels_get(): %s\n",
+			strerror( errno ) );
+		return rc_queues;
+	}
+
+	log_msg( LOG_NOTICE, "xdp.max_queues=%u xdp.combined_queues=%u\n", max_queues,
+		combined_queues );
+
 	return 0;
 }
 
--- xdp-server.h
+++ xdp-server.h
@@ -4,6 +4,8 @@
 #ifndef _NSD_XDP_SERVER_
 #define _NSD_XDP_SERVER_
 
+#define XDP_ETHER_ADDR_LEN 6
+
 typedef struct xdp_server_options {
 	char const* _interface_name;
 	int _queue_count;
@@ -13,6 +15,8 @@ typedef struct xdp_server_options {
 typedef struct xdp_server {
 	xdp_server_options_type _options;
 	region_type* _region;
+	int _interface_index;
+	uint8_t _interface_hardware_address[XDP_ETHER_ADDR_LEN];
 } xdp_server_type;
 
 int xdp_server_init( xdp_server_type* xdp );
