f81140ab728c488ebef9d57e81813fe3dc4b11d7
mb <mb@64k.by>
2020-10-16 10:54:28 +0200

    xdp: add templates for the server part

---
+++ xdp-server.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2020, NLnet Labs. All rights reserved.
+// See LICENSE for the details
+
+#include "config.h"
+
+#include "xdp-dissect.h"
+#include "xdp-server.h"
+
+#include <stdint.h>
+
+#include <bpf/bpf.h>
+
---
+++ xdp-server.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, NLnet Labs. All rights reserved.
+// See LICENSE for the details
+
+#ifndef _NSD_XDP_SERVER_
+#define _NSD_XDP_SERVER_
+
+
+#endif
