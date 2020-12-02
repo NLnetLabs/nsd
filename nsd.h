2e1d6f80c82f58ded6ffe454d40845afb2fca17e
Sara Dickinson <sara@sinodun.com>
2020-12-02 13:22:27 +0000

    tidy tabs vs spaces

--- nsd.h
+++ nsd.h
@@ -268,8 +268,8 @@ struct	nsd
 	int tcp_query_count;
 	int tcp_timeout;
 	int tcp_idle_timeout;
-    int xfrd_conn_reuse;
-    
+	int xfrd_conn_reuse;
+
 	int tcp_mss;
 	int outgoing_tcp_mss;
 	size_t ipv4_edns_size;
