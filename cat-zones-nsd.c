9b819e89f35337798a0dde442d52ba3a4dc41007
Koen <koen@koenvh.nl>
2023-04-11 14:29:32 +0200

    Start checks for catz requirements

--- cat-zones-nsd.c
+++ cat-zones-nsd.c
@@ -47,7 +47,13 @@ catz_add_zone(const catz_dname *member_zone_name,
 	nsd_type* nsd = (nsd_type*)arg;
 	const char* zname = dname_to_string(&member_zone_name->dname, NULL);
 	const char* pname = zname;
-	zone_type* t = find_or_create_zone(nsd->db, &member_zone_name->dname, nsd->options, zname, pname);
+	zone_type* t = find_or_create_zone(
+		nsd->db, 
+		&member_zone_name->dname, 
+		nsd->options, 
+		zname, 
+		pname
+	);
 
 	if (t) {
 		return CATZ_SUCCESS;
@@ -73,11 +79,71 @@ void nsd_catalog_consumer_process(struct nsd *nsd, struct zone *zone)
 	struct zone_rr_iter rr_iter;
 	struct rr *rr;
 
+	uint8_t has_version_txt = 0;
+	
+	dname_type* previous_ptrs = (dname_type*)malloc(0 * sizeof(dname_type));
+	uint32_t previous_ptrs_no = 0;
+	
+
 	DEBUG(DEBUG_CATZ, 1, (LOG_INFO, "TODO: Catalog zone processing"));
 	zone_rr_iter_init(&rr_iter, zone);
 	for ( rr = zone_rr_iter_next(&rr_iter)
 	    ; rr != NULL
 	    ; rr = zone_rr_iter_next(&rr_iter)) {
+			if (
+				rr->type == TYPE_TXT && 
+				rr->klass == CLASS_IN && 
+				strcasecmp(
+					dname_to_string(rr->owner->dname, zone->apex->dname),
+					 "version"
+				) == 0
+			) {
+				DEBUG(DEBUG_CATZ, 1, 
+				(LOG_INFO, "Catz version TXT"));
+				if (has_version_txt) {
+					DEBUG(DEBUG_CATZ, 1, 
+					(LOG_INFO, "Catz has more than one version TXT defined"));
+				} else if (
+					rr->rdata_count != 1 || 
+					strcmp((char*)rr->rdatas[0].data, "\"2\"") != 0
+				) {
+					// TODO: Fix check for 2
+					DEBUG(DEBUG_CATZ, 1, 
+					(LOG_INFO, "Catz has a version different than 2"));
+					DEBUG(DEBUG_CATZ, 1, (LOG_INFO, rr->rdatas[1].data));
+				} else {
+					has_version_txt = 2;
+				}
+			}
+			// Maybe also check whether an NS record is present, although it is 
+			// not really breaking anything when it fails.
+			if (rr->type == TYPE_PTR && rr->klass == CLASS_IN) {
+				char* label = dname_to_string(
+					rr->owner->dname, 
+					zone->apex->dname
+				);
+				DEBUG(DEBUG_CATZ, 1, 
+				(LOG_INFO, label));
+
+				dname_type* ptrs[previous_ptrs_no + 1];
+				for (uint32_t i = 0; i < previous_ptrs_no; i++) {
+					if (dname_compare(
+						rr->owner->dname, 
+						&previous_ptrs[i]
+					) == 0) {
+						DEBUG(DEBUG_CATZ, 1, 
+						(LOG_INFO, "Catz more than one PTR record for this label"));
+					}
+					ptrs[i] = &previous_ptrs[i];
+				}
+				ptrs[previous_ptrs_no] = rr->owner->dname;
+				free(previous_ptrs);
+				previous_ptrs = 
+					(dname_type*)malloc(previous_ptrs_no + 1 * sizeof(dname_type));
+
+				previous_ptrs = &ptrs;
+				previous_ptrs_no++;
+			}
 		DEBUG(DEBUG_CATZ, 1, (LOG_INFO, "TODO: Process RR"));
 	}
 }
