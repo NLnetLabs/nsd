5f5426f792d86e322254057141da1e01d342d5c2
Jared Mauch <jared@puck.nether.net>
2025-08-03 11:17:19 -0400

    ixfr can result in nsec3 hash tree null node deletion

--- rbtree.c
+++ rbtree.c
@@ -64,6 +64,11 @@ static void
 rbtree_rotate_left(rbtree_type *rbtree, rbnode_type *node)
 {
 	rbnode_type *right = node->right;
+
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return;
+	}
 	node->right = right->left;
 	if (right->left != RBTREE_NULL)
 		right->left->parent = node;
@@ -91,6 +96,11 @@ static void
 rbtree_rotate_right(rbtree_type *rbtree, rbnode_type *node)
 {
 	rbnode_type *left = node->left;
+
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return;
+	}
 	node->left = left->right;
 	if (left->right != RBTREE_NULL)
 		left->right->parent = node;
@@ -114,6 +124,11 @@ static void
 rbtree_insert_fixup(rbtree_type *rbtree, rbnode_type *node)
 {
 	rbnode_type	*uncle;
+
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return;
+	}
 
 	/* While not at the root and need fixing... */
 	while (node != rbtree->root && node->parent->color == RED) {
@@ -186,6 +201,11 @@ rbtree_insert (rbtree_type *rbtree, rbnode_type *data)
 	/* XXX Not necessary, but keeps compiler quiet... */
 	int r = 0;
 
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return NULL;
+	}
+
 	/* We start at the root of the tree */
 	rbnode_type	*node = rbtree->root;
 	rbnode_type	*parent = RBTREE_NULL;
@@ -237,6 +257,11 @@ rbtree_search (rbtree_type *rbtree, const void *key)
 {
 	rbnode_type *node;
 
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return NULL;
+	}
+
 	if (rbtree_find_less_equal(rbtree, key, &node)) {
 		return node;
 	} else {
@@ -257,6 +282,11 @@ static void swap_np(rbnode_type** x, rbnode_type** y)
 
 static void change_parent_ptr(rbtree_type* rbtree, rbnode_type* parent, rbnode_type* old, rbnode_type* new)
 {
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return;
+	}
+	
 	if(parent == RBTREE_NULL)
 	{
 		assert(rbtree->root == old);
@@ -280,6 +310,12 @@ rbtree_delete(rbtree_type *rbtree, const void *key)
 {
 	rbnode_type *to_delete;
 	rbnode_type *child;
+	
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return NULL;
+	}
+	
 	if((to_delete = rbtree_search(rbtree, key)) == 0) return 0;
 	rbtree->count--;
 
@@ -357,6 +393,11 @@ static void rbtree_delete_fixup(rbtree_type* rbtree, rbnode_type* child, rbnode_
 {
 	rbnode_type* sibling;
 	int go_up = 1;
+
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return;
+	}
 
 	/* determine sibling to the node that is one-black short */
 	if(child_parent->right == child) sibling = child_parent->left;
@@ -464,6 +505,12 @@ rbtree_find_less_equal(rbtree_type *rbtree, const void *key, rbnode_type **resul
 
 	assert(result);
 
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		*result = NULL;
+		return 0;
+	}
+
 	/* We start at root... */
 	node = rbtree->root;
 
@@ -497,6 +544,11 @@ rbtree_first (rbtree_type *rbtree)
 {
 	rbnode_type *node;
 
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return NULL;
+	}
+
 	for (node = rbtree->root; node->left != RBTREE_NULL; node = node->left);
 	return node;
 }
@@ -505,6 +557,11 @@ rbnode_type *
 rbtree_last (rbtree_type *rbtree)
 {
 	rbnode_type *node;
+
+	/* Check if rbtree is NULL */
+	if (!rbtree) {
+		return NULL;
+	}
 
 	for (node = rbtree->root; node->right != RBTREE_NULL; node = node->right);
 	return node;
