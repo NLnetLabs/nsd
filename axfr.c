5699868f60307623ebec0d29ea713016dfd04477
W.C.A. Wijngaards <wouter@nlnetlabs.nl>
2026-01-15 11:04:56 +0100

    - Fix #475 info: axfr for domain from not-verified.

--- axfr.c
+++ axfr.c
@@ -256,10 +256,10 @@ static int axfr_ixfr_can_admit_query(struct nsd* nsd, struct query* q)
 		return 0;
 	}
 #ifdef HAVE_SSL
-	DEBUG(DEBUG_XFRD,1, (LOG_INFO, "%s admitted acl %s %s %s",
+	DEBUG(DEBUG_XFRD,1, (LOG_INFO, "%s admitted acl %s %s%s",
 		(q->qtype==TYPE_AXFR?"axfr":"ixfr"),
 		acl->ip_address_spec, acl->key_name?acl->key_name:"NOKEY",
-		(q->tls||q->tls_auth)?(q->tls?"tls":"tls-auth"):""));
+		(q->tls||q->tls_auth)?(q->tls?" tls":" tls-auth"):""));
 #else
 	DEBUG(DEBUG_XFRD,1, (LOG_INFO, "%s admitted acl %s %s",
 		(q->qtype==TYPE_AXFR?"axfr":"ixfr"),
@@ -269,11 +269,12 @@ static int axfr_ixfr_can_admit_query(struct nsd* nsd, struct query* q)
 		char a[128];
 		addr2str(&q->client_addr, a, sizeof(a));
 #ifdef HAVE_SSL
-		VERBOSITY(1, (LOG_INFO, "%s for %s from %s %s %s",
+		VERBOSITY(1, (LOG_INFO, "%s for %s from %s%s%s%s",
 			(q->qtype==TYPE_AXFR?"axfr":"ixfr"),
 			dname_to_string(q->qname, NULL), a,
-			(q->tls||q->tls_auth)?(q->tls?"tls":"tls-auth"):"",
-			q->cert_cn?q->cert_cn:"not-verified"));
+			(q->tls||q->tls_auth)?(q->tls?" tls":" tls-auth"):"",
+			(q->tls||q->tls_auth)?" ":"",
+			(q->tls||q->tls_auth)?(q->cert_cn?q->cert_cn:"not-verified"):""));
 #else
 		VERBOSITY(1, (LOG_INFO, "%s for %s from %s",
 			(q->qtype==TYPE_AXFR?"axfr":"ixfr"),
